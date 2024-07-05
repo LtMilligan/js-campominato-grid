7069856fdf2785c0d181068a32ce1fd2566d5971
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 18:17:39 +0200

    [UPD] Adding click event for SQUARE

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -4,6 +4,10 @@ const btnStart = document.getElementById('btn-start')
 btnStart.addEventListener('click', function() {
     for (let i = 0; i < 100; i++) {
         let currentSquare = createSquare()
+        currentSquare.addEventListener('click', function() {
+            this.classList.toggle('clicked')
+            console.log(currentSquare.innerText)
+        })
         currentSquare.innerText = i + 1;
     }
 
