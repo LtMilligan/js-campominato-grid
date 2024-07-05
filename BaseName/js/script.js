a89c07fdb93c91a1262687b8b1a1a4c50e98adbd
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 18:23:52 +0200

    [FIX] Change folder name

---
+++ BaseName/js/script.js
@@ -0,0 +1,22 @@
+const grid = document.getElementById('grid');
+const btnStart = document.getElementById('btn-start')
+
+btnStart.addEventListener('click', function() {
+    for (let i = 0; i < 100; i++) {
+        let currentSquare = createSquare()
+        currentSquare.addEventListener('click', function() {
+            this.classList.toggle('clicked')
+            console.log(currentSquare.innerText)
+        })
+        currentSquare.innerText = i + 1;
+        grid.append(currentSquare)
+    }
+
+})
+
+
+function createSquare() {
+    let currentElement = document.createElement('div');
+    currentElement.classList.add('square');
+    return currentElement
+}
