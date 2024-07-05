7fd53ee01d3777b0558c2fbf8f61a2dbd8acc97f
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 18:16:58 +0200

    [UPD] Adding innerText for SQUARE with number iteration + 1

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -4,6 +4,7 @@ const btnStart = document.getElementById('btn-start')
 btnStart.addEventListener('click', function() {
     for (let i = 0; i < 100; i++) {
         let currentSquare = createSquare()
+        currentSquare.innerText = i + 1;
     }
 
 })
