78d390124062b32613cf7bca880c28ab8d61ba61
LtMilligan <gioele.miscia@gmail.com>
2024-07-05 18:14:47 +0200

    [UPD] Adding var for creation function

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -3,7 +3,7 @@ const btnStart = document.getElementById('btn-start')
 
 btnStart.addEventListener('click', function() {
     for (let i = 0; i < 100; i++) {
-    
+        let currentSquare = createSquare()
     }
 
 })
